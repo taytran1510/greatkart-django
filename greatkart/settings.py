f4ccc3e668860a152332e1cecb091517ee82a5d2
Thuan Tran Nhu <thuantran0164@gmail.com>
2022-06-09 20:19:42 +0700

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
 
 ROOT_URLCONF = 'greatkart.urls'
@@ -133,3 +135,19 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '18133054@student.hcmute.edu.vn'
+EMAIL_HOST_PASSWORD = 'thuantrannhu12'
+EMAIL_USE_TLS = True
+# EMAIL_TIMEOUT = 3
+
+# LOGIN_REDIRECT_URL = ''
+# SESSION_EXPIRE_SECONDS = 60
