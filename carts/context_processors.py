bbfec8a055de768e9360e07d18109b2f86067cb2
Thuan Tran Nhu <thuantran0164@gmail.com>
2022-06-12 16:29:56 +0700

    handled cart and checkout page for the logged in users

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -8,7 +8,16 @@ def counter(request):
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            if request.user.is_authenticated:
+                #if user authenticate valid --> get value from this account user that saved in database
+                cart_items = CartItem.objects.all().filter(user=request.user)
+                print('counter for cart 1!')
+                print(cart_items)
+            else:
+                # else we get data from request cart id
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
+                print('counter for cart 2!')
+                print(cart_items)
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
         except Cart.DoesNotExist:
